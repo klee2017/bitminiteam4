c5c496c4edb75b46833b15a3eabffebacaf969cb
이현규 <xnxnznzn@gmail.com>
2018-09-27 10:22:32 +0900

    Member 테이블 및 시퀀스 생성

---
+++ makeFriendsTravel/project.sql
@@ -0,0 +1,14 @@
+create table tb_member(
+    mem_no number primary key,
+    mem_id varchar2(30) not null,
+    mem_name varchar2(15) not null,
+    mem_pass varchar2(30) not null,
+    mem_pass_hint varchar2(30) not null,
+    mem_gender char,
+    mem_favorite_place varchar2(20),
+    mem_reg_date date,
+    mem_poto VARCHAR2(100)
+)
+
+create SEQUENCE m_member_no
+  INCREMENT BY 1 MAXVALUE 5000 CYCLE;

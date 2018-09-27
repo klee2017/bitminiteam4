3ffb88723e2fdadcb85887ff91246acf044f0851
DESKTOP-KAY\kay <kaythechemist@gmail.com>
2018-09-27 11:28:53 +0900

    review board mapper 작성

--- makeFriendsTravel/src/main/java/kr/co/tt/repository/mapper/ReviewBoardMapper.java
+++ makeFriendsTravel/src/main/java/kr/co/tt/repository/mapper/ReviewBoardMapper.java
@@ -2,10 +2,14 @@ package kr.co.tt.repository.mapper;
 
 import java.util.List;
 
-import kr.co.tt.repository.domain.Board;
+import kr.co.tt.repository.domain.ReviewBoard;
 
 
 
 public interface ReviewBoardMapper {
-	
+	List<ReviewBoard> selectBoard();
+	ReviewBoard selectBoardByNo(int no);
+	void insertBoard(ReviewBoard board);
+	void deleteBoard(int no);
+	void updateBoard(ReviewBoard board);
 }

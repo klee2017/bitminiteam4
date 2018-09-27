2e9900ee4eae6359c5f27a4461b9c61339d12933
DESKTOP-KAY\kay <kaythechemist@gmail.com>
2018-09-27 11:04:14 +0900

    review board domain VO 작성

---
+++ makeFriendsTravel/src/main/java/kr/co/tt/repository/domain/ReviewBoard.java
@@ -0,0 +1,58 @@
+package kr.co.tt.repository.domain;
+
+import java.util.Date;
+
+public class ReviewBoard {
+	int no;
+	String title;
+	String content;
+	Date regDate;
+	Date modDate;
+	int recCnt;
+	int memNo;
+	
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public Date getModDate() {
+		return modDate;
+	}
+	public void setModDate(Date modDate) {
+		this.modDate = modDate;
+	}
+	public int getRecCnt() {
+		return recCnt;
+	}
+	public void setRecCnt(int recCnt) {
+		this.recCnt = recCnt;
+	}
+	public int getMemNo() {
+		return memNo;
+	}
+	public void setMemNo(int memNo) {
+		this.memNo = memNo;
+	}
+	
+}
--- makeFriendsTravel/tb_review.sql
+++ makeFriendsTravel/tb_review.sql
@@ -1,16 +1,32 @@
+create table tb_member(
+    mem_no number primary key,
+    mem_id varchar2(30) not null,
+    mem_name varchar2(15) not null,
+    mem_pass varchar2(30) not null,
+    mem_pass_hint varchar2(30) not null,
+    mem_gender char,
+    mem_favorite_place varchar2(20),
+    mem_reg_date date,
+    mem_poto VARCHAR2(100)
+);
+
 create table tb_board_review (
     no number primary key,
     title varchar2(200) not null,
     content varchar2(4000) not null,
     reg_date date default sysdate,
     mod_date date default sysdate,
-    rec_cnt number default 0
---    mem_no number REFERENCES 
+    rec_cnt number default 0,
+    mem_no number REFERENCES tb_member(mem_no)
 );
 
 create sequence s_board_review;
 
 drop sequence s_board_review;
+
+drop table tb_board_review purge;
+
+drop table tb_member purge;
 
 select *
   from tb_board_review
@@ -36,7 +52,8 @@ create table tb_comment_review (
     comment_content varchar2(420),
     comment_reg_date date default sysdate,
     comment_mod_date date default sysdate,
-    no number REFERENCES tb_board_review(no)
+    no number REFERENCES tb_board_review(no),
+    mem_no number REFERENCES tb_member(mem_no)
 );
 
 create sequence s_comment_review;
